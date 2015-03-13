094dc98453151315e8b5d524c40edccd467eebd1
Your full name <joshua.hampp@ipa.fraunhofer.de>
2015-03-13 06:28:05 +0100

    update to adb

--- serodi_app/src/adb_connect.py
+++ serodi_app/src/adb_connect.py
@@ -6,6 +6,9 @@ import os, time
 
 if __name__ == '__main__':
 	while True:
+		os.system("adb shell am force-stop com.icecoldapps.sshserver")
+		os.system("adb shell am start -n com.icecoldapps.sshserver/.viewStart")
 		os.system("adb forward tcp:2222 tcp:2222")
+		time.sleep(5)
 		os.system("(ssh -R 9090:localhost:9090 serodi@localhost -p 2222 & );ssh -R 38300:localhost:38300 serodi@localhost -p 2222")
 		time.sleep(0.2)
