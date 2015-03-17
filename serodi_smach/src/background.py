2b6d644e4dec60e833ab74afef45fadf2b6e6cad
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-03-17 08:51:20 +0100

    started with restart base

--- serodi_smach/src/background.py
+++ serodi_smach/src/background.py
@@ -10,13 +10,16 @@ import states.movement
 
 global sub_js
 
-def recover_base():
-    rospy.wait_for_service('/base/recover')
+def trigger_srv(srv):
+    rospy.wait_for_service(srv)
     try:
-        recover = rospy.ServiceProxy('/base/recover', cob_srvs.srv.Trigger)
+        recover = rospy.ServiceProxy(srv, cob_srvs.srv.Trigger)
         return recover()
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
+        
+def recover_base():
+	trigger_srv('/base/recover')
 
 needs_recover = False
 def cb_em(data):
@@ -33,6 +36,14 @@ def cb_js(data):
 	sub_js.unregister()
 	
 	os.system("roslaunch serodi_smach spawner.launch &")
+	
+def restart_base():
+	global sub_js
+	
+	print "TODO base shutdown"
+	trigger_srv('/base/recover')
+	sub_js = rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState, cb_js)
+	trigger_srv('/base/init')
 
 if __name__ == "__main__":
 	rospy.init_node('background', anonymous=True)
