55b18d2a3551dafd23186a0ad356533ebb0301e8
Your full name <joshua.hampp@ipa.fraunhofer.de>
2015-03-17 13:23:26 +0100

    started...

--- serodi_app/src/adb_connect.py
+++ serodi_app/src/adb_connect.py
@@ -4,6 +4,9 @@ import os, time
 #make sure adb is working and login works without password!
 #tested with ssh server for android
 
+def shutdown():
+	os.system("adb shell reboot -p") #does not work on my device
+
 if __name__ == '__main__':
 	while True:
 		os.system("adb shell am force-stop com.icecoldapps.sshserver")
