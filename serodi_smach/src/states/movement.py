2ceab466d3b5bb6fd3862cf55cddaeca77e43e67
Your full name <joshua.hampp@ipa.fraunhofer.de>
2015-03-18 09:12:47 +0100

    added some own reg.

--- serodi_smach/src/states/movement.py
+++ serodi_smach/src/states/movement.py
@@ -57,6 +57,167 @@ class MoveToPose(smach.State):
 		except:
 			return 'failed'
 		return 'success'
+		
+
+class MoveRel(smach.State):
+    def __init__(self, sss, motion):
+        smach.State.__init__(self, outcomes=['success', 'failed'], input_keys=['text','nonblocking'])
+        self.sss = sss
+        self.pose = motion
+
+    def execute(self, userdata):
+		try:
+			h = self.sss.move_base_rel('base', self.motion)
+			if (not hasattr(userdata, 'nonblocking') or userdata.nonblocking==False):
+				h.wait()
+		except:
+			return 'failed'
+		return 'success'
+		
+import sensor_msgs.msg
+class ReadDataForRegistration(smach.State):
+    def __init__(self, dest_name):
+        smach.State.__init__(self, outcomes=['pass'], input_keys=['data'], output_keys=['data'])
+        self.dest_name = dest_name.split('.')
+        
+    def on_data(self, data):
+		self.res_angle = data.angle_increment
+		self.data = data.ranges
+			
+    def wait_for_data(self):
+		self.data = None
+		while self.data==None and not rospy.is_shutdown():
+			time.sleep(0.1)
+		return self.data
+
+    def execute(self, userdata):
+		self.sub = rospy.Subscriber("/somescan", sensor_msgs.msg.LaserScan, self.on_data)
+		
+		s = [userdata.data]
+		for dd in self.dest_name[0:len(self.dest_name)-1]:
+			s=[s[0][dd]]
+		data = self.wait_for_data()
+		for d in data:
+			s[0][self.dest_name[len(self.dest_name)-1]].append(d)
+		
+		self.sub.unregister()
+		return 'pass'
+		
+class MoveRel_Registration(smach.State):
+    def __init__(self, sss, data, max_angle = 20./180*math.pi, max_trans=0.3):
+        smach.State.__init__(self, outcomes=['success', 'failed'], input_keys=['text','nonblocking'])
+        self.sss = sss
+        self.teachin = data
+        self.max_angle = max_angle
+        self.max_trans = max_trans
+        self.max_matching_dist = max_trans*1.2
+        
+    def on_data(self, data):
+		self.res_angle = data.angle_increment
+		self.data = data.ranges
+			
+    def get_angle(self, data):
+		if len(self.teachin)!=len(data):
+			smach.logerr("wrong data size")
+			return None
+						
+		max_i = int(round(self.max_angle/self.res_angle+0.9))
+		best = []
+		for o in range(-max_i,max_i+1):
+			energy = 0
+			matched = 0
+			for i in xrange(len(data)):
+				j = (len(data)+i+o)%len(data)
+				delta = abs(data[i]-self.teachin[j])
+				if data[i]>0 and self.teachin[j]>0 and delta<self.max_matching_dist:
+					energy += delta
+					matched += 1
+			print o, "matched ",matched
+			if matched<0.3*len(data):
+				continue
+			energy /= matched
+			print "energy ",energy
+			
+			if len(best)==0 or best[0]>energy:
+				best = [energy, o]
+				
+		if len(best)==0:
+			smach.logerr("could not find any match")
+			return None
+			
+		angle = best[1]*self.res_angle
+		print "o -> ", o, angle
+		
+		return angle
+			
+    def get_trans(self, data):
+		if len(self.teachin)!=len(data):
+			smach.logerr("wrong data size")
+			return None
+						
+		sdx = 0
+		sdy = 0
+		matched = 0
+		for i in xrange(len(data)):
+			delta = data[i]-self.teachin[i]
+			if data[i]>0 and self.teachin[i]>0 and abs(delta)<self.max_matching_dist:
+				a = i*self.res_angle
+				sdx += math.cos(a)*delta
+				sdy += math.sin(a)*delta
+				matched += 1
+		print "matched ",matched
+		if matched<0.3*len(data):
+			smach.logerr("could not find enough matches")
+			return None
+			
+		return [sdx/matched, sdy/matched]
+			
+    def wait_for_data(self):
+		self.data = None
+		while self.data==None and not rospy.is_shutdown():
+			time.sleep(0.1)
+		return self.data
+		
+    def move(self,m):
+		print "going to move relative by ",m
+		
+		#h = self.sss.move_base_rel('base', m)
+		#if (not hasattr(userdata, 'nonblocking') or userdata.nonblocking==False):
+		#	h.wait()
+
+    def execute(self, userdata):
+		print self.teachin
+		self.sub = rospy.Subscriber("/somescan", sensor_msgs.msg.LaserScan, self.on_data)
+		
+		if True:#try:
+			
+			#recover rotation
+			s = 0
+			while s<self.max_angle and not rospy.is_shutdown():
+				angle = self.get_angle(self.wait_for_data())
+				if angle==None: raise
+				if abs(angle)<0.03: break
+				self.move([0,0,angle])
+				s+=abs(angle)
+				
+			
+			#recover translation
+			s = 0
+			while s<self.max_trans and not rospy.is_shutdown():
+				trans = self.get_trans(self.wait_for_data())
+				if trans==None: raise
+				
+				dist = math.sqrt( trans[0]*trans[0] + trans[1]*trans[1] )
+				if dist<0.03: break
+				
+				self.move([trans[0],trans[1],0])
+				s += dist
+			
+		#except:
+		#	self.sub.unregister()
+		#	return 'failed'
+		self.sub.unregister()
+		return 'success'
 
 
 import cob_srvs.srv, tf
