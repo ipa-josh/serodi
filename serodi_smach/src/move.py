678efdaf6b9a2b1de7e1e62402fea7e0a7f725b1
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-03-19 08:51:41 +0100

    added helper

---
+++ serodi_smach/src/move.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import roslib; roslib.load_manifest('serodi_smach')
+import rospy
+import smach
+import smach_ros
+
+from simple_script_server import simple_script_server
+
+import states.interaction
+import states.movement
+
+# main
+def main():
+    sss = simple_script_server()
+    rospy.init_node('test_reg')
+
+    # Create a SMACH state machine
+    logger = states.interaction.SMACHCustomLogger()
+    sm = smach.StateMachine(outcomes=['success', 'failure'])
+    
+    sm.userdata.data = {'scan':[]}
+    sm.userdata.nonblocking = False
+
+    # Open the container
+    with sm:
+        smach.StateMachine.add('MoveRel', states.movement.MoveRel(sss, [-0.1,0.0,0.]), 
+                               transitions={'success':'success', 'failed':'failure'})
+                                            
+    # Execute SMACH plan
+    outcome = sm.execute()
+    print "SMACH resulted in ",outcome
+
+
+if __name__ == '__main__':
+    main()
